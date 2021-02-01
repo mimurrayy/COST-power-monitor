6b00852cd7ddc0992f282f6d3c40ba946ffdc521
Julian Held <julian.held@gmail.com>
2021-02-01 20:07:05 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cost-power-monitor",
-    version="1.1.0",
+    version="1.1.1",
     author="Julian Held",
     author_email="julian.held@rub.de",
     license='MIT',
